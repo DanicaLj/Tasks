e1560a9c68a5e216c62104c06d3a7f6554ae333e
DanicaLj <55446720+DanicaLj@users.noreply.github.com>
2023-02-10 23:55:22 +0100

    update file

--- task4/authorizeApi.php
+++ task4/authorizeApi.php
@@ -4,7 +4,7 @@
 	header('Access-Control-Allow-Headers: X-Requested-With');
 	header('Content-Type: application/json');
 
-	//require_once('vendor/autoload.php');
+	require_once('vendor/autoload.php');
 	include_once 'database.php';
 	include_once 'customer.php';
 
@@ -15,11 +15,10 @@
 	$authorization = isset($headers['Authorization']) ? $headers['Authorization'] : null;
 	$authorizationToken = $db->getBearerToken($authorization);
 
-	// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-	// $dotenv->load();
-	// $_ENV['AUTHORIZATION']
+	$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+	$dotenv->load();
 
-	if($authorizationToken && $authorizationToken == 'danica') {
+	if($authorizationToken && $authorizationToken == $_ENV['AUTHORIZATION']) {
 		
 		if ($api == 'POST') {
 			$postData = json_decode(file_get_contents('php://input'),true);
